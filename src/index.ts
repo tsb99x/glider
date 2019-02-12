5a10fa77e1b44614a1b4b6f066615b3e66a5c271
Anton Muravyov <tsb99x@gmail.com>
2019-02-12 21:35:52 +0300

    Disable play controls on resize, Expand grid before randomization

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,22 @@
 import { identify } from './util'
 import { Automata, initAutomata, drawActiveGrid, doPause, doStart, doStep, randomizeAutomata } from './automata'
 
-// Automata element.
-
-let auto: Automata | null = null
-
-// Layout setup.
-
 const container: HTMLCanvasElement = identify('container')
 const info: HTMLElement = identify('info')
 const grid: HTMLCanvasElement = identify('grid')
-
-// Dynamic text parts and resize.
-
-const xSize: HTMLSpanElement = identify('x-size')
-const ySize: HTMLSpanElement = identify('y-size')
-const totalSize: HTMLSpanElement = identify('total-size')
-
-function expandGrid() {
-    const width = container.clientWidth
-    const height = container.clientHeight
-
-    grid.width = width
-    grid.height = height
-
-    xSize.innerText = width.toString()
-    ySize.innerText = height.toString()
-    totalSize.innerText = (width * height).toString()
-
-    if (auto) {
-        doPause(auto)
-    }
-
-    updateViews(false)
-}
-
-expandGrid()
-window.addEventListener('resize', expandGrid)
-
-// Button identification and context-binding.
 
 const btnRand: HTMLButtonElement = identify('btn-rand')
 const btnStart: HTMLButtonElement = identify('btn-start')
 const btnStep: HTMLButtonElement = identify('btn-step')
 const btnPause: HTMLButtonElement = identify('btn-pause')
+
+const xSize: HTMLSpanElement = identify('x-size')
+const ySize: HTMLSpanElement = identify('y-size')
+const totalSize: HTMLSpanElement = identify('total-size')
+
+let auto: Automata | null = null
+
+// Global update actions.
 
 function updateViews(isGrid: boolean) {
     if (isGrid) {
@@ -61,13 +34,42 @@ function updateButtons(isAutomataRunning: boolean) {
     btnPause.disabled = !isAutomataRunning
 }
 
-// Button actions.
+function disableButtons() {
+    btnStart.disabled = true
+    btnStep.disabled = true
+    btnPause.disabled = true
+}
+
+function expandGrid() {
+    const width = container.clientWidth
+    const height = container.clientHeight
+
+    grid.width = width
+    grid.height = height
+
+    xSize.innerText = width.toString()
+    ySize.innerText = height.toString()
+    totalSize.innerText = (width * height).toString()
+
+    if (auto) {
+        doPause(auto)
+    }
+
+    updateViews(false)
+    disableButtons()
+}
+
+expandGrid()
+window.addEventListener('resize', expandGrid)
+
+// Button-specific binds.
 
 btnRand.addEventListener('click', () => {
     if (auto) {
         doPause(auto)
     }
 
+    expandGrid()
     auto = initAutomata(grid)
     randomizeAutomata(auto)
     drawActiveGrid(auto)
